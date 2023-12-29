ddfaa02751215fbba7c6883f6db5cfb446827934
Maxsoll159 <mar_1905@hotmail.es>
2023-12-28 23:51:50 -0500

    Video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -10,8 +10,8 @@ export const Video = () => {
         <p className="text-center text-base md:text-xl font-semibold">En Acuivetlife aspiramos a ser líderes en el mercado de la acuicultura, reconocidos por nuestra excelencia, compromiso con la sostenibilidad y contribución al desarrollo de una industria acuícola más responsable y próspera.</p>
         <button className="font-bold text-lg p-3 mt-6 hover:scale-105 transition-all ease-out border rounded-lg bg-white text-[#030f23]">Saber Mas</button>
       </div>
-      <video style={{ clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)" }} className="w-full object-cover object-top h-full" src="/videos/video1.mp4" autoPlay muted loop></video>
-      <video className="absolute -bottom-48 blur-3xl -z-40" src="/videos/video1.mp4" autoPlay muted loop></video>
+      <video style={{ clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)" }} className="w-full object-cover object-top h-full" src="/videos/video1.webm" autoPlay muted loop></video>
+      <video className="absolute -bottom-48 blur-3xl -z-40" src="/videos/video1.webm" autoPlay muted loop></video>
     </div>
   )
 }
